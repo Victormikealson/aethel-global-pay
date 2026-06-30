9bf7fbe76bd36857cc2ccc6d550d89cfa07c552f
Victormikealson <victormikealson@github.com>
2026-06-30 13:38:28 +0300

    Fix adaptive icon SDK requirement - set minSdk 26, use mipmap icon

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         applicationId = "com.aethel.aethel_global_pay"
-        minSdk = flutter.minSdkVersion
+        minSdk = 26
         targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
